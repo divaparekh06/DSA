25ccfcf65293b328bff1f8c52f36d8c9311605c1
Diva Parekh <parekh.diva06@gmail.com>
2026-08-11 23:13:42 +0530

    Time: 1 ms (25.02%), Space: 71.8 MB (15.17%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maximum = nums[0];
+        int sum = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            sum = max(nums[i],sum+nums[i]);
+            maximum = max(maximum,sum);
+            // if(sum<0){
+            //     sum==0;     
+            // }
+        }
+        return maximum;
+        
+    }
+};
